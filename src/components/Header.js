86a3c50e1eefc427d981e5c05f31c772ee4f71c5
AndreBeco <anfelipebeco@gmail.com>
2025-08-04 21:43:46 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -87,7 +87,7 @@ const Header = ({ currentPage, onNavigate }) => {
           
           <div className="flex items-center">
             <img
-              src="https://i.pinimg.com/736x/4b/9d/c7/4b9dc7bae3ffc1d8ecbfe4ea994026dc.jpg"
+              src="https://i.pinimg.com/736x/74/f7/62/74f7627d0cdab08531fa9b4ba542e05e.jpg"
               alt="Logo Muebles DisOffice"
               className="h-24 sm:h-28 w-auto object-contain"
             />
