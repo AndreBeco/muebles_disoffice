afa2ef996e85765436591dac94ad25897a1f1b16
AndreBeco <anfelipebeco@gmail.com>
2025-09-08 17:49:24 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -86,7 +86,7 @@ const Header = ({ currentPage, onNavigate }) => {
             <img
               src="https://i.pinimg.com/736x/3b/be/bf/3bbebfccff85353ee6d91a28083ffbc9.jpg"
               alt="Logo Muebles DisOffice"
-              className="h-24 sm:h-28 w-auto object-contain"
+              className="w-auto h-24 sm:h-28 object-contain"
             />
           </div>
 
