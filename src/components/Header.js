0e656790d1c52c71972df5fff3b8b373f93e295a
AndreBeco <anfelipebeco@gmail.com>
2025-09-08 17:33:43 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -80,20 +80,14 @@ const Header = ({ currentPage, onNavigate }) => {
 
       {/* Main Header */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-24 lg:h-28">
-          {/* IZQUIERDA: Logo (como lo tenías) */}
-          <div className="flex items-center space-x-3">
+        <div className="flex justify-between items-center h-20 lg:h-24">
+          {/* Logo grande */}
+          <div className="flex items-center">
             <img
               src="https://i.pinimg.com/736x/3b/be/bf/3bbebfccff85353ee6d91a28083ffbc9.jpg"
               alt="Logo Muebles DisOffice"
-              className="w-24 h-24 sm:w-28 sm:h-28 object-contain"
-              referrerPolicy="no-referrer"
+              className="w-40 h-40 sm:w-48 sm:h-48 object-contain"
             />
-            {/* Si no quieres el texto al lado, borra este bloque */}
-            <div className="text-2xl lg:text-3xl font-bold text-blue-900 leading-tight">
-              <span className="text-blue-600">Muebles</span>
-              <span className="text-gray-800">DisOffice</span>
-            </div>
           </div>
 
           {/* CENTRO: Navegación desktop */}
