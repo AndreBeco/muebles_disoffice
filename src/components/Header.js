121a7816e896754f22b9fc27df50faf30704d555
AndreBeco <anfelipebeco@gmail.com>
2025-09-08 18:11:19 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -64,6 +64,7 @@ const Header = ({ currentPage, onNavigate }) => {
               <div className="flex items-center space-x-2">
                 <Phone className="w-4 h-4" />
                 <span>+57 318 406 3943</span>
+                <span>+57 317 711 0447</span>
               </div>
               <div className="flex items-center space-x-2">
                 <Mail className="w-4 h-4" />
